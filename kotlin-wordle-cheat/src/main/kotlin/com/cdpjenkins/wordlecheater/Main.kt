9ef4ba7c7ca3a9b7f9963bb2bb19fa779ebfd49a
Chris Jenkins <cdpjenkins@gmail.com>
2025-01-28 22:39:10 +0000

    Show number of possibilities as we whitle down the words in main

--- kotlin-wordle-cheat/src/main/kotlin/com/cdpjenkins/wordlecheater/Main.kt
+++ kotlin-wordle-cheat/src/main/kotlin/com/cdpjenkins/wordlecheater/Main.kt
@@ -5,15 +5,17 @@ import java.io.File
 fun main(args: Array<String>) {
     val cheater = WordleCheater()
 
-    args.toList().windowed(size = 2, step = 2).forEach { (attempt, result) ->
-        println("$attempt $result")
-
-        cheater.guess(attempt, result)
-    }
-
     val dictionaryWords = File("/usr/share/dict/words").readLines().filter { it.length == 5 }
 
-    val possibleWords = dictionaryWords.filter { cheater.matches(it) }
+    args.toList().windowed(size = 2, step = 2).forEach { (attempt, result) ->
+        cheater.guess(attempt, result)
 
+        val possibleWords = dictionaryWords.filter { cheater.matches(it) }
+        println("$attempt $result ... ${possibleWords.size} possible solutions")
+
+        println(possibleWords.size)
+    }
+
+    val possibleWords = dictionaryWords.filter { cheater.matches(it) }
     println(possibleWords)
 }
