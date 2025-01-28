2be629c4849ad6689816631184fd4b11a532cac8
Chris Jenkins <cdpjenkins@gmail.com>
2025-01-28 22:39:09 +0000

    Plug in a main method
    It's not complete, but it might be enough for a lot of Wordles.
    
    Let the cheating begin!

--- kotlin-wordle-cheat/src/main/kotlin/Main.kt
+++ kotlin-wordle-cheat/src/main/kotlin/Main.kt
@@ -1,7 +1,20 @@
 package org.example
 
+import com.cdpjenkins.wordlecheater.WordleCheater
 import java.io.File
 
 fun main(args: Array<String>) {
-    println(args.toList())
+    val cheater = WordleCheater()
+
+    args.toList().windowed(size = 2, step = 2).forEach { (attempt, result) ->
+        println("$attempt $result")
+
+        cheater.oneRound(attempt, result)
+    }
+
+    val dictionaryWords = File("/usr/share/dict/words").readLines().filter { it.length == 5 }
+
+    val possibleWords = dictionaryWords.filter { cheater.matches(it) }
+
+    println(possibleWords)
 }
