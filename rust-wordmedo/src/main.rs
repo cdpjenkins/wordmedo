c097890cd581b97bfe556bb841e663ca523c9b3b
Chris Jenkins <chris.jenkins@masabi.com>
2025-03-18 21:28:12 +0000

    Starting to do basic Rust things

--- rust-wordmedo/src/main.rs
+++ rust-wordmedo/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
+
+    let file_contents = std::fs::read_to_string("/usr/share/dict/words").unwrap();
+
+    let lines = file_contents.lines();
+
+
+    let possible_words: Vec<&str>  = lines.filter(|l| { l.len() == 5}).collect();
+
+    for word in &possible_words {
+        println!("{word}");
+    }
+
+    println!("There are {} possible words", possible_words.len());
+
 }
